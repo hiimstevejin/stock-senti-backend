9ec473fc742fce09476e7e013f8e546b173bbef6
hiimstevejin <jingle5695@gmail.com>
2025-07-02 22:15:08 -0500

    prepare deploy2

--- server/settings.py
+++ server/settings.py
@@ -87,10 +87,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-    )
+    'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
 }
 
 # Password validation
